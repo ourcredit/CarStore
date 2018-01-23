7d45684d04d5ccb040e2249760904d97ab4cb6df
zhaohejing2017 <zhaohejing@zhj125.cn>
2018-01-23 11:27:37 +0800

    wwww

--- src/YT.Web/Assets/Dashboard/src/components/Trees/Mtree.vue
+++ src/YT.Web/Assets/Dashboard/src/components/Trees/Mtree.vue
@@ -2,153 +2,208 @@
     <Tree :data="data5" :render="renderContent"></Tree>
 </template>
 <script>
-    export default {
-        data () {
-            return {
-                data5: [
-                    {
-                        title: 'parent 1',
-                        expand: true,
-                        render: (h, { root, node, data }) => {
-                            return h('span', {
-                                style: {
-                                    display: 'inline-block',
-                                    width: '100%'
-                                }
-                            }, [
-                                h('span', [
-                                    h('Icon', {
-                                        props: {
-                                            type: 'ios-folder-outline'
-                                        },
-                                        style: {
-                                            marginRight: '8px'
-                                        }
-                                    }),
-                                    h('span', data.title)
-                                ]),
-                                h('span', {
-                                    style: {
-                                        display: 'inline-block',
-                                        float: 'right',
-                                        marginRight: '32px'
-                                    }
-                                }, [
-                                    h('Button', {
-                                        props: Object.assign({}, this.buttonProps, {
-                                            icon: 'ios-plus-empty',
-                                            type: 'primary'
-                                        }),
-                                        style: {
-                                            width: '52px'
-                                        },
-                                        on: {
-                                            click: () => { this.append(data) }
-                                        }
-                                    })
-                                ])
-                            ]);
-                        },
-                        children: [
-                            {
-                                title: 'child 1-1',
-                                expand: true,
-                                children: [
-                                    {
-                                        title: 'leaf 1-1-1',
-                                        expand: true
-                                    },
-                                    {
-                                        title: 'leaf 1-1-2',
-                                        expand: true
-                                    }
-                                ]
-                            },
-                            {
-                                title: 'child 1-2',
-                                expand: true,
-                                children: [
-                                    {
-                                        title: 'leaf 1-2-1',
-                                        expand: true
-                                    },
-                                    {
-                                        title: 'leaf 1-2-1',
-                                        expand: true
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ],
-                buttonProps: {
-                    type: 'ghost',
-                    size: 'small',
-                }
-            }
-        },
-        methods: {
-            renderContent (h, { root, node, data }) {
-                return h('span', {
-                    style: {
-                        display: 'inline-block',
-                        width: '100%'
-                    }
-                }, [
-                    h('span', [
-                        h('Icon', {
-                            props: {
-                                type: 'ios-paper-outline'
-                            },
-                            style: {
-                                marginRight: '8px'
-                            }
-                        }),
-                        h('span', data.title)
-                    ]),
-                    h('span', {
-                        style: {
-                            display: 'inline-block',
-                            float: 'right',
-                            marginRight: '32px'
-                        }
-                    }, [
-                        h('Button', {
-                            props: Object.assign({}, this.buttonProps, {
-                                icon: 'ios-plus-empty'
-                            }),
-                            style: {
-                                marginRight: '8px'
-                            },
-                            on: {
-                                click: () => { this.append(data) }
-                            }
-                        }),
-                        h('Button', {
-                            props: Object.assign({}, this.buttonProps, {
-                                icon: 'ios-minus-empty'
-                            }),
-                            on: {
-                                click: () => { this.remove(root, node, data) }
-                            }
-                        })
-                    ])
-                ]);
-            },
-            append (data) {
-                const children = data.children || [];
-                children.push({
-                    title: 'appended node',
-                    expand: true
-                });
-                this.$set(data, 'children', children);
-            },
-            remove (root, node, data) {
-                const parentKey = root.find(el => el === node).parent;
-                const parent = root.find(el => el.nodeKey === parentKey).node;
-                const index = parent.children.indexOf(data);
-                parent.children.splice(index, 1);
-            }
-        }
+export default {
+  name: "m-tree",
+  props: {
+    /*查询Api,方法*/
+    getTree: {
+      type: Function,
+      required: true
+    },
+    /*新增编辑方法*/
+    insertOrUpdate: {
+      type: Function,
+      required: false
     }
+  },
+  data() {
+    return {
+      list: [],
+      data5: [
+        {
+          title: "parent 1",
+          expand: true,
+          render: (h, { root, node, data }) => {
+            return h(
+              "span",
+              {
+                style: {
+                  display: "inline-block",
+                  width: "100%"
+                }
+              },
+              [
+                h("span", [
+                  h("Icon", {
+                    props: {
+                      type: "ios-folder-outline"
+                    },
+                    style: {
+                      marginRight: "8px"
+                    }
+                  }),
+                  h("span", data.title)
+                ]),
+                h(
+                  "span",
+                  {
+                    style: {
+                      display: "inline-block",
+                      float: "right",
+                      marginRight: "32px"
+                    }
+                  },
+                  [
+                    h("Button", {
+                      props: Object.assign({}, this.buttonProps, {
+                        icon: "ios-plus-empty",
+                        type: "primary"
+                      }),
+                      style: {
+                        width: "52px"
+                      },
+                      on: {
+                        click: () => {
+                          this.append(data);
+                        }
+                      }
+                    })
+                  ]
+                )
+              ]
+            );
+          },
+          children: [
+            {
+              title: "child 1-1",
+              expand: true,
+              children: [
+                {
+                  title: "leaf 1-1-1",
+                  expand: true
+                },
+                {
+                  title: "leaf 1-1-2",
+                  expand: true
+                }
+              ]
+            },
+            {
+              title: "child 1-2",
+              expand: true,
+              children: [
+                {
+                  title: "leaf 1-2-1",
+                  expand: true
+                },
+                {
+                  title: "leaf 1-2-1",
+                  expand: true
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      buttonProps: {
+        type: "ghost",
+        size: "small"
+      }
+    };
+  },
+  created() {
+    this.initData();
+  },
+  methods: {
+    //初始化
+    async initData() {
+      try {
+        this.getApiData();
+      } catch (err) {
+        this.$Message.error("对方不想说话，并且向你抛出了一个异常");
+      }
+    },
+    /*获取数据*/
+    getApiData() {
+      this.list = [];
+      this.getTree().then(response => {
+        var result = response.data;
+        this.list = (result && result.result && result.result.items) || [];
+      });
+    },
+    renderContent(h, { root, node, data }) {
+      return h(
+        "span",
+        {
+          style: {
+            display: "inline-block",
+            width: "100%"
+          }
+        },
+        [
+          h("span", [
+            h("Icon", {
+              props: {
+                type: "ios-paper-outline"
+              },
+              style: {
+                marginRight: "8px"
+              }
+            }),
+            h("span", data.title)
+          ]),
+          h(
+            "span",
+            {
+              style: {
+                display: "inline-block",
+                float: "right",
+                marginRight: "32px"
+              }
+            },
+            [
+              h("Button", {
+                props: Object.assign({}, this.buttonProps, {
+                  icon: "ios-plus-empty"
+                }),
+                style: {
+                  marginRight: "8px"
+                },
+                on: {
+                  click: () => {
+                    this.append(data);
+                  }
+                }
+              }),
+              h("Button", {
+                props: Object.assign({}, this.buttonProps, {
+                  icon: "ios-minus-empty"
+                }),
+                on: {
+                  click: () => {
+                    this.remove(root, node, data);
+                  }
+                }
+              })
+            ]
+          )
+        ]
+      );
+    },
+    append(data) {
+      const children = data.children || [];
+      children.push({
+        title: "appended node",
+        expand: true
+      });
+      this.$set(data, "children", children);
+    },
+    remove(root, node, data) {
+      const parentKey = root.find(el => el === node).parent;
+      const parent = root.find(el => el.nodeKey === parentKey).node;
+      const index = parent.children.indexOf(data);
+      parent.children.splice(index, 1);
+    }
+  }
+};
 </script>
